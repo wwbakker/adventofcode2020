0e21731257ac073fb0a9caf55e2a1b9a8e95a2d0
Wessel W. Bakker <wwbakker@gmail.com>
2020-12-04 19:12:53 +0100

    - Fixed a bug where it would consider the same entry multiple times. https://github.com/w8mr

--- src/main/scala/Day1B.scala
+++ src/main/scala/Day1B.scala
@@ -4,11 +4,11 @@ object Day1B extends App {
 
   def sumTo2020() : Option[Int] = {
     val f = file"./files/day1/input"
-    val entries : Iterable[Int] = f.lines.map(_.toInt)
+    val entries : Iterable[(Int, Int)] = f.lines.map(_.toInt).zipWithIndex
     for {
-      a <- entries
-      b <- entries
-      c <- entries if a + b + c == 2020
+      (a, i1) <- entries
+      (b, i2) <- entries
+      (c, i3) <- entries if a + b + c == 2020 && i1 != i2 && i2 != i3 && i1 != i3
     } yield a * b * c
   }.headOption
 
