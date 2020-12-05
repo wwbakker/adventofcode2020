da6b9f236cf3d43d5887cabbd41f7f75e53721dc
Wessel W. Bakker <wwbakker@gmail.com>
2020-12-05 12:03:10 +0100

    - Added day 5

---
+++ src/main/scala/Day5B.scala
@@ -0,0 +1,58 @@
+import better.files._
+
+import scala.annotation.tailrec
+
+object Day5B extends App {
+  val f = file"./files/day5/input"
+  val numRows = 128
+  val numColumns = 8
+
+  sealed trait Instruction
+  case object Lower extends Instruction
+  case object Higher extends Instruction
+
+  @tailrec
+  def determineIndex(lowerBoundIndex: Int, length: Int, pendingInstructions: List[Instruction]): Int =
+    pendingInstructions match {
+      case Nil if length != 1 => throw new IllegalStateException("Something went wrong, not enough instruction")
+      case _ :: _ if length == 1 => throw new IllegalStateException("Something went wrong, too many instructions")
+      case Nil => lowerBoundIndex
+      case Lower :: xs => determineIndex(lowerBoundIndex, length / 2, xs)
+      case Higher :: xs => determineIndex(lowerBoundIndex + (length / 2), length / 2, xs)
+    }
+
+  case class SeatInstructions(rowInstructions: List[Instruction], columnInstructions: List[Instruction]) {
+    lazy val rowIndex: Int = determineIndex(0, numRows, rowInstructions)
+    lazy val columnIndex: Int = determineIndex(0, numColumns, columnInstructions)
+
+    def seatId : Int =
+       rowIndex * 8 + columnIndex
+  }
+
+  object SeatInstructions {
+
+    def parseRowInstruction(c : Char) : Instruction = c match {
+      case 'F' => Lower
+      case 'B' => Higher
+      case _ => throw new IllegalArgumentException("Unexpected character when parsing row")
+    }
+
+    def parseColumnInstruction(c : Char) : Instruction = c match {
+      case 'L' => Lower
+      case 'R' => Higher
+      case _ => throw new IllegalArgumentException("Unexpected character when parsing column")
+    }
+
+    def parse(s: String): SeatInstructions =
+      SeatInstructions(
+        rowInstructions = s.substring(0, 7).map(parseRowInstruction).toList,
+        columnInstructions = s.substring(7, 10).map(parseColumnInstruction).toList
+      )
+  }
+
+  val seatIds: Seq[Int] = f.lines.map(SeatInstructions.parse).map(_.seatId).toSeq.sorted
+  val missingSeatId = (seatIds.min to seatIds.max).find(!seatIds.contains(_))
+
+  println("Day5A:")
+  println(s"Missing seat id: $missingSeatId")
+}
