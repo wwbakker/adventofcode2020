ae97dea1cf98c42f0d656c5e6d542afba64fe705
Wessel W. Bakker <wwbakker@gmail.com>
2020-12-11 20:11:13 +0100

    - Method that works, but very inefficient.

--- src/main/scala/Day10B.scala
+++ src/main/scala/Day10B.scala
@@ -1,42 +1,24 @@
 import better.files._
 
-import scala.annotation.tailrec
-
 object Day10B extends App {
-  val f = file"./files/day10/example2"
+  val f = file"./files/day10/input"
 
-  val adapters: Iterable[Int] = f.lines.map(_.toInt).toSeq
-  val steps = (Seq(adapters.max) ++ adapters).toSet
+  val adapters: Seq[Int] = f.lines.map(_.toInt).toSeq
+  val output = adapters.max + 3
+  val steps = (0 +: adapters).toSet
 
-  @tailrec
-  def optimalSet(currentSet : Set[Int], currentE : Int) : Set[Int] =
-    if (steps.contains(currentE + 3))
-      optimalSet(currentSet.incl(currentE + 3), currentE + 3)
-    else if (steps.contains(currentE + 2))
-      optimalSet(currentSet.incl(currentE + 2), currentE + 2)
-    else if (steps.contains(currentE + 1))
-      optimalSet(currentSet.incl(currentE + 1), currentE + 1)
-    else if (steps.max > currentE)
-      throw new IllegalArgumentException("Cannot reach the highest adapter")
+  def numberOfWays(n : Int) : Int = {
+    if (n == 0) 1
     else
-      currentSet
-
-  val difference = steps.diff(optimalSet(Set.empty, 0))
-  val variations = Math.pow(2, difference.size)
-
-  println("Day10a:")
-  println(difference.toSeq.sorted)
-  println(variations)
-
-//  val count: Count = adapters.foldLeft(Count(0,0,0))((count, e) =>
-//    e - count.previousJoltage match {
-//      case 1 => Count(e, count.oneDifference + 1, count.threeDifference)
-//      case 0 | 2 => Count(e, count.oneDifference, count.threeDifference)
-//      case 3 => Count(e, count.oneDifference, count.threeDifference + 1)
-//      case _ => throw new IllegalArgumentException("Too much difference")
-//    }
-//  )
+      (if (steps.contains(n - 1)) numberOfWays(n - 1) else 0) +
+        (if (steps.contains(n - 2)) numberOfWays(n - 2) else 0) +
+        (if (steps.contains(n - 3)) numberOfWays(n - 3) else 0)
+  }
 
 
+  val now = numberOfWays(output)
+
+  println("Day10B:")
+  println(now)
 
 }
