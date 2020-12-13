9c1db46689ddfec4ef6fb442f09bfd12e976f567
Wessel W. Bakker <wwbakker@gmail.com>
2020-12-13 15:48:12 +0100

    A bit faster but not enough.

--- src/main/scala/Day13B.scala
+++ src/main/scala/Day13B.scala
@@ -1,16 +1,18 @@
 import better.files._
 
 object Day13B extends App{
-  val f = file"./files/day13/example"
+  val f = file"./files/day13/input"
 
   type Index = Int
 
-  case class LongRange(step : Long) {
+  case class LongRange(step : Long, start : Long = 0L) {
     def find(func : Long => Boolean) : Option[Long] =
-      Iterator.iterate(0L)(_+step).find(func)
+      Iterator.iterate(start)(_+step).find(func)
     def contains(v : Long) : Boolean =
       v % step == 0
 
+    def skip(n : Long) : LongRange =
+      LongRange(step, n - (n % step))
   }
 
   val busIds: Seq[(Index, LongRange)] = f.lines.toList match {
@@ -20,17 +22,17 @@ object Day13B extends App{
         .filterNot(_._1=="x")
         .map{case(busId, index) =>
           (index, LongRange(busId.toLong))}
-//        .sortBy(_._2.step).reverse
+        .sortBy(_._2.step).reverse
         .toList
   }
 
   val t = busIds match {
-    case (_, firstBusRange) :: otherBusses =>
-      firstBusRange.find(time =>
-        otherBusses.forall{ case (otherBusIndex, otherBusRange) =>
-          otherBusRange.contains(time + otherBusIndex)
-        }
-      )
+    case (firstBusIndex, firstBusRange) :: otherBusses =>
+      firstBusRange.skip(100000000000000L).find(time =>
+          otherBusses.forall { case (otherBusIndex, otherBusRange) =>
+            otherBusRange.contains(time - firstBusIndex + otherBusIndex)
+          }
+      ).map(_-firstBusIndex)
   }
 
   println("Day13B:")
