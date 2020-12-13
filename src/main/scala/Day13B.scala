c99ed5c957caed714bab079d1cde53f0bcda850d
Wessel W. Bakker <wwbakker@gmail.com>
2020-12-13 12:50:39 +0100

    Working version but too slow.

--- src/main/scala/Day13B.scala
+++ src/main/scala/Day13B.scala
@@ -1,19 +1,26 @@
 import better.files._
 
-import scala.collection.immutable.NumericRange
-
 object Day13B extends App{
   val f = file"./files/day13/example"
 
   type Index = Int
 
-  val busIds: Seq[(Index, NumericRange.Inclusive[Index])] = f.lines.toList match {
+  case class LongRange(step : Long) {
+    def find(func : Long => Boolean) : Option[Long] =
+      Iterator.iterate(0L)(_+step).find(func)
+    def contains(v : Long) : Boolean =
+      v % step == 0
+
+  }
+
+  val busIds: Seq[(Index, LongRange)] = f.lines.toList match {
     case _ :: secondLine :: Nil =>
       secondLine.split(",")
         .zipWithIndex
         .filterNot(_._1=="x")
         .map{case(busId, index) =>
-          (index, NumericRange.inclusive(0, Int.MaxValue, busId.toInt))}
+          (index, LongRange(busId.toLong))}
+//        .sortBy(_._2.step).reverse
         .toList
   }
 
